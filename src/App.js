cb19599be462c3cbe4505978ea41779672f69db9
Karan <karanchandekar431@gmail.com>
2023-01-14 19:37:34 +0530

    added home page with image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from "react";
+import Home from "./components/Home";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Home />
+    </React.Fragment>
+  );
+};
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <React.Fragment>
+      <div className="w-full h-screen relative">
+        <img
+          src="images/mesuem.jpg"
+          alt=""
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Home;
