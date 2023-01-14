931287b752ea73918e164356627cd7279df3ed19
Karan <karanchandekar431@gmail.com>
2023-01-14 19:46:30 +0530

    created navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillTwitterSquare,
+  AiOutlineSearch,
+} from "react-icons/ai";
 
 const Home = () => {
   return (
@@ -10,6 +16,25 @@ const Home = () => {
           className="w-full h-full object-cover"
         />
       </div>
+
+      <nav className="">
+        <div>
+          <h1>قطر</h1>
+        </div>
+
+        <div>
+          <ul>
+            <li>Doha</li>
+            <li>Al Wakrah</li>
+            <li>Al Rayan</li>
+            <li>Al Khor</li>
+          </ul>
+          <div>
+            <AiOutlineSearch />
+            <input type="text" />
+          </div>
+        </div>
+      </nav>
     </React.Fragment>
   );
 };
