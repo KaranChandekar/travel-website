743e86d637b5e919ae1e8d468ac27a755ab520f3
Karan <karanchandekar431@gmail.com>
2023-01-14 21:37:18 +0530

    created places head

--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Places = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <h1>Places not to be missed</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, a
+          quibusdam. Et nostrum accusamus sunt fugiat, corrupti aut sint totam.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Places;
