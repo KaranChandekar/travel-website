0d890b9f477c2e771f4027bd0339840a7523bf01
Karan <karanchandekar431@gmail.com>
2023-01-14 22:22:31 +0530

    created and styled our recommedation section

--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { AiFillStar } from "react-icons/ai";
 
 const Places = () => {
   return (
@@ -11,7 +12,7 @@ const Places = () => {
         </p>
       </div>
 
-      <div className="block md:grid grid-rows-2 grid-flow-col gap-4 w-full h-[300px] md:h-[450px] pb-10 border-b">
+      <div className="block md:grid grid-rows-2 grid-flow-col gap-4 w-full pb-10 border-b">
         <div className="row-span-1 col-span-2">
           <img
             className="w-full h-full object-cover rounded-md"
@@ -19,7 +20,6 @@ const Places = () => {
             alt=""
           />
         </div>
-
         <div className="row-span-1 col-span-1">
           <img
             className="w-full h-full object-cover rounded-md"
@@ -27,7 +27,6 @@ const Places = () => {
             alt=""
           />
         </div>
-
         <div className="row-span-1 col-span-1">
           <img
             className="w-full h-full object-cover rounded-md"
@@ -35,7 +34,6 @@ const Places = () => {
             alt=""
           />
         </div>
-
         <div className="row-span-2 col-span-1">
           <img
             className="w-full h-full object-cover rounded-md"
@@ -43,7 +41,6 @@ const Places = () => {
             alt=""
           />
         </div>
-
         <div className="row-span-1 col-span-1">
           <img
             className="w-full h-full object-cover rounded-md"
@@ -51,7 +48,6 @@ const Places = () => {
             alt=""
           />
         </div>
-
         <div className="row-span-1 col-span-1">
           <img
             className="w-full h-full object-cover rounded-md"
@@ -59,6 +55,204 @@ const Places = () => {
             alt=""
           />
         </div>
+      </div>
+
+      <div className="my-10 text-center">
+        <h1 className="text-4xl font-bold">Our Recomendation</h1>
+        <section className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-cols-1 gap-10 my-10 justify-items-center items-center">
+          <div className="drop-shadow-2xl text-left rounded space-y-2 bg-white">
+            <img
+              className="w-full h-1/2 object-cover rounded-t-lg"
+              src="images/image-8.jpg"
+              alt=""
+            />
+            <div className="p-4 space-y-4">
+              <div className="flex justify-between">
+                <p className="text-sm text-red-400">Lorem, ipsum.</p>
+                <p className="font-bold">$500</p>
+              </div>
+              <p className="font-semibold">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Facere!
+              </p>
+              <div className="flex justify-between items-center">
+                <h2>15 days tour</h2>
+                <p className="flex items-center space-x-2 text-orange-400">
+                  <AiFillStar />
+                  <span>4.8</span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="drop-shadow-2xl text-left rounded space-y-2 bg-white">
+            <img
+              className="w-full h-1/2 object-cover rounded-t-lg"
+              src="images/image-3.jpg"
+              alt=""
+            />
+            <div className="p-4 space-y-4">
+              <div className="flex justify-between">
+                <p className="text-sm text-red-400">Lorem, ipsum.</p>
+                <p className="font-bold">$500</p>
+              </div>
+              <p className="font-semibold">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Facere!
+              </p>
+              <div className="flex justify-between items-center">
+                <h2>15 days tour</h2>
+                <p className="flex items-center space-x-2 text-orange-400">
+                  <AiFillStar />
+                  <span>4.8</span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="drop-shadow-2xl text-left rounded space-y-2 bg-white">
+            <img
+              className="w-full h-1/2 object-cover rounded-t-lg"
+              src="images/image-2.jpg"
+              alt=""
+            />
+            <div className="p-4 space-y-4">
+              <div className="flex justify-between">
+                <p className="text-sm text-red-400">Lorem, ipsum.</p>
+                <p className="font-bold">$500</p>
+              </div>
+              <p className="font-semibold">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Facere!
+              </p>
+              <div className="flex justify-between items-center">
+                <h2>15 days tour</h2>
+                <p className="flex items-center space-x-2 text-orange-400">
+                  <AiFillStar />
+                  <span>4.8</span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="drop-shadow-2xl text-left rounded space-y-2 bg-white">
+            <img
+              className="w-full h-1/2 object-cover rounded-t-lg"
+              src="images/image-7.jpg"
+              alt=""
+            />
+            <div className="p-4 space-y-4">
+              <div className="flex justify-between">
+                <p className="text-sm text-red-400">Lorem, ipsum.</p>
+                <p className="font-bold">$500</p>
+              </div>
+              <p className="font-semibold">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Facere!
+              </p>
+              <div className="flex justify-between items-center">
+                <h2>15 days tour</h2>
+                <p className="flex items-center space-x-2 text-orange-400">
+                  <AiFillStar />
+                  <span>4.8</span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="drop-shadow-2xl text-left rounded space-y-2 bg-white">
+            <img
+              className="w-full h-1/2 object-cover rounded-t-lg"
+              src="images/image-8.jpg"
+              alt=""
+            />
+            <div className="p-4 space-y-4">
+              <div className="flex justify-between">
+                <p className="text-sm text-red-400">Lorem, ipsum.</p>
+                <p className="font-bold">$500</p>
+              </div>
+              <p className="font-semibold">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Facere!
+              </p>
+              <div className="flex justify-between items-center">
+                <h2>15 days tour</h2>
+                <p className="flex items-center space-x-2 text-orange-400">
+                  <AiFillStar />
+                  <span>4.8</span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="drop-shadow-2xl text-left rounded space-y-2 bg-white">
+            <img
+              className="w-full h-1/2 object-cover rounded-t-lg"
+              src="images/image-3.jpg"
+              alt=""
+            />
+            <div className="p-4 space-y-4">
+              <div className="flex justify-between">
+                <p className="text-sm text-red-400">Lorem, ipsum.</p>
+                <p className="font-bold">$500</p>
+              </div>
+              <p className="font-semibold">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Facere!
+              </p>
+              <div className="flex justify-between items-center">
+                <h2>15 days tour</h2>
+                <p className="flex items-center space-x-2 text-orange-400">
+                  <AiFillStar />
+                  <span>4.8</span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="drop-shadow-2xl text-left rounded space-y-2 bg-white">
+            <img
+              className="w-full h-1/2 object-cover rounded-t-lg"
+              src="images/image-2.jpg"
+              alt=""
+            />
+            <div className="p-4 space-y-4">
+              <div className="flex justify-between">
+                <p className="text-sm text-red-400">Lorem, ipsum.</p>
+                <p className="font-bold">$500</p>
+              </div>
+              <p className="font-semibold">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Facere!
+              </p>
+              <div className="flex justify-between items-center">
+                <h2>15 days tour</h2>
+                <p className="flex items-center space-x-2 text-orange-400">
+                  <AiFillStar />
+                  <span>4.8</span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="drop-shadow-2xl text-left rounded space-y-2 bg-white">
+            <img
+              className="w-full h-1/2 object-cover rounded-t-lg"
+              src="images/image-7.jpg"
+              alt=""
+            />
+            <div className="p-4 space-y-4">
+              <div className="flex justify-between">
+                <p className="text-sm text-red-400">Lorem, ipsum.</p>
+                <p className="font-bold">$500</p>
+              </div>
+              <p className="font-semibold">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Facere!
+              </p>
+              <div className="flex justify-between items-center">
+                <h2>15 days tour</h2>
+                <p className="flex items-center space-x-2 text-orange-400">
+                  <AiFillStar />
+                  <span>4.8</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
