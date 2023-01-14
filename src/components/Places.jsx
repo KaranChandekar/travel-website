1310e95b489824feeaf6fdd75c361a4861e48f50
Karan <karanchandekar431@gmail.com>
2023-01-14 21:47:18 +0530

    styled places head

--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Places = () => {
   return (
-    <div>
-      <div>
-        <h1>Places not to be missed</h1>
+    <div className="w-4/5 m-auto cursor-default">
+      <div className="w-full m-auto text-center my-10 space-y-5">
+        <h1 className="text-4xl font-bold">Places not to be missed</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, a
           quibusdam. Et nostrum accusamus sunt fugiat, corrupti aut sint totam.
