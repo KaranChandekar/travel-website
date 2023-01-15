6daf3a6381a8a3829589e6eea76ecc9a2bc89f41
Karan <karanchandekar431@gmail.com>
2023-01-15 13:36:22 +0530

    update discover style

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Discover = () => {
   return (
-    <React.Fragment>
+    <div className="w-4/5 m-auto cursor-default">
       <h1 className="text-4xl font-bold text-center">Discover</h1>
-      <div className="w-4/5 xl:w-9/12 m-auto flex flex-col sm:flex-row space-y-10 md:space-x-10 justify-between items-center py-10">
-        <div className="w-[500px] space-y-5">
+      <div className="w-full m-auto flex flex-col md:flex-row space-y-10 md:space-x-10 justify-between items-center py-10">
+        <div className="w-full space-y-5">
           <img
-            className="drop-shadow-2xl rounded-lg border-8 border-stones-700"
+            className="w-full drop-shadow-2xl rounded-lg border-8 border-stones-700"
             src="images/safari_desert.jpg"
             alt=""
           />
@@ -30,7 +30,7 @@ const Discover = () => {
             laboriosam!
           </p>
         </div>
-        <div className="w-[500px] space-y-4 text-left">
+        <div className="w-full space-y-4 text-left">
           <h2 className="font-bold">Lorem ipsum dolor sit.</h2>
           <h1 className="font-bold text-2xl">Lorem ipsum dolor sitamet.</h1>
           <p className="text-sm">
@@ -92,7 +92,7 @@ const Discover = () => {
           />
         </div>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
